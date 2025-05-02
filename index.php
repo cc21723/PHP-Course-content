7f1e907d913ab3096d2a8313a6f9d333212ca960
mackliu <macklun@gmail.com>
2025-05-02 08:54:55 +0800

    增加專案首頁進行各頁面的連結

--- index.php
+++ index.php
@@ -1,26 +1,36 @@
-<?php
-
-define("PI",3.14159);
-
-$age=51;
-$name="劉勤永";
-$ename="mackliu";
-
-echo '我的年齡是：' . $age . "歲";
-echo "<br>";
-echo "我的名字是：".$name;
-echo "<br>";
-echo PI;
-
-echo "<hr>";
-
-$age=36;
-$name="張大頭";
-
-echo '我的年齡是：' . $age . "歲";
-echo "<br>";
-echo "我的名字是：".$name;
-echo "<br>";
-echo PI;
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>程式基礎</title>
+     <style>
+        /* ul{
+            list-style-type:none;
+            display:flex;
+            justify-content:center;
+            flex-wrap:wrap;
+            width:70%;
+            padding:0 5%;
+            
+        }
+        li{
+            min-width:100px;
+            padding:10px 20px;
+            border:1px solid blue;
+            border-radius:15px;
+            box-shadow:1px 1px 5px #eee;
+            margin:1%;
+        } */
+     </style>
+</head>
+<body>
+    <ul>
+        <li><a href="var.php">變數</a></li>
+        <li><a href="./select/index.php">判斷式</a></li>
+        <li><a href="./loop/index.php">迴圈-重覆結構</a></li>
+        <li><a href="./array/index.php">陣列</a></li>
+        <li><a href="./array/index.php">字串</a></li>
+    </ul>
+</body>
+</html>
